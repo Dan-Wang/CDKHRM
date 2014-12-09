c80343be6123478c2d1aec7afc8031d296ded7b3
dinner3000 <dinner3000@hotmail.com>
2014-12-09 18:18:32 +0800

    Initial commit Signed-off-by: dinner3000 <dinner3000@hotmail.com>

---
+++ lib/confs/Conf.php
@@ -0,0 +1,33 @@
+<?php
+
+class Conf {
+
+    var $smtphost;
+    var $dbhost;
+    var $dbport;
+    var $dbname;
+    var $dbuser;
+    var $version;
+
+    function Conf() {
+
+        $this->dbhost = 'localhost';
+        $this->dbport = '3306';
+        if (defined('ENVIRNOMENT') && (ENVIRNOMENT == 'dev' || ENVIRNOMENT == 'test')) {
+            $this->dbname = 'test_orangehrm_mysql';
+            $this->dbuser = 'root';
+            $this->dbpass = '1234abcd';
+        } else {
+            $this->dbname = 'orangehrm_mysql';
+            $this->dbuser = 'root';
+            $this->dbpass = 'hrmcdk';
+        }
+        $this->version = '3.2.1';
+
+        $this->emailConfiguration = dirname(__FILE__) . '/mailConf.php';
+        $this->errorLog = realpath(dirname(__FILE__) . '/../logs/') . '/';
+    }
+
+}
+
+?>
